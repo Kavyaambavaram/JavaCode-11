df0811874bfd2a4ea0c9a692714aa125c413848f
Kavya <Kavya@DESKTOP-7815M31>
2024-01-23 13:22:58 +0530

    Collection Framework

---
+++ CollectionFramework/src/com/wrapperClassess/Test.java
@@ -0,0 +1,13 @@
+package com.wrapperClassess;
+
+public class Test {
+
+	public static void main(String[] args) {
+		int data=10;
+		Integer obj=new Integer(data);
+		System.out.println(obj instanceof java.lang.Integer);
+		
+
+	}
+
+}
